37fd214bfa7a471a6bceef90fe199282daefbd96
Gautham-sketch <82305202+Gautham-sketch@users.noreply.github.com>
2022-08-13 22:39:58 +0530

    Add files via upload

---
+++ components/treasure.js
@@ -0,0 +1,29 @@
+AFRAME.registerComponent("coins",{
+    init: function(){
+        for(var i=1; i<=10; i++){
+            var id = `coin${i}`
+            var posX = Math.random()*50 + (-30)
+            var posY = Math.random()*1 + (-1)
+            var posZ = Math.random()*30 + (-10)
+            var position = {x:posX, y:posY, z:posZ}
+            this.createCoins(id,position)
+        }
+    },
+
+    createCoins: function(id,position){
+        var treasureEntity = document.querySelector("#id1")
+        var coins = document.createElement("a-entity")
+        coins.setAttribute("id",id)
+        coins.setAttribute("position",position)
+        coins.setAttribute("material","color","yellow")
+        coins.setAttribute("geometry",{primitive:"circle",radius:0.25})
+        coins.setAttribute("animation",{
+            property : "rotation",
+            to : "0 360 0",
+            loop : "true",
+            dur : 1000
+        });
+        
+        treasureEntity.appendChild(coins)
+    }
+})
